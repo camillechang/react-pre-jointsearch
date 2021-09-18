44f5560319722c6034bed8c420525b1f8344472c
Camille Chang <hong13626@gmail.com>
2021-09-18 17:05:25 +1000

    add antd, craco for styled-compoent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { ProjectSearchList } from "./screens/project-list/index";
-import './App.css';
+// import "./App.css";
+import "./App.less";
 
 function App() {
   return (
     <div className="App">
- <ProjectSearchList />
+      <ProjectSearchList />
     </div>
   );
 }
--- src/screens/project-list/list.jsx
+++ src/screens/project-list/list.jsx
@@ -1,23 +1,50 @@
+import { Table } from "antd";
+
 export const List = ({ list, managers }) => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Project Name</th>
-          <th>Manager</th>
-        </tr>
-      </thead>
-      <tbody>
-        {list.map((project) => (
-          <tr key={project.id}>
-            <td>{project.name}</td>
-            <td>
-              {managers.find((manager) => manager.id === project.manager_id)
-                ?.name || "unknown"}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+  const columns = [{
+    title: "ProjectName",
+    dataIndex: "ProjectName",
+    key: "ProjectName"
+  },
+  {
+    title: "Manager",
+    dataIndex: "Manager",
+    key: "Manager"
+  }
+  ]
+
+  const data = []
+  list.map((project) => {
+    const info = {
+      key: project.id,
+      ProjectName: project.name,
+      Manager: managers.find((manager) => manager.id === project.manager_id)?.name || "unknown"
+    }
+    data.push(info);
+  });
+
+
+  return <Table columns={columns} dataSource={data} />;
+
+  // return (
+  //   <table>
+  //     <thead>
+  //       <tr>
+  //         <th>Project Name</th>
+  //         <th>Manager</th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>
+  //       {list.map((project) => (
+  //         <tr key={project.id}>
+  //           <td>{project.name}</td>
+  //           <td>
+  //             {managers.find((manager) => manager.id === project.manager_id)
+  //               ?.name || "unknown"}
+  //           </td>
+  //         </tr>
+  //       ))}
+  //     </tbody>
+  //   </table>
+  // );
 };
